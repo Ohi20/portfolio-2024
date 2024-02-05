3f2c4397e7d547919d35c35e8830fe3d133ab3df
AshrafOhi <ashrafohi20@gmail.com>
2024-02-05 16:11:41 +0600

    resume-pdf-added

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -48,7 +48,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/dummy.pdf"
+              href="/web-developer-resume-ashraf-ohi.pdf"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
